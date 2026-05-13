6602e2a305eae2e204f55b2a1c9097163ad96f33
Sun Pei Fu <sunpeifu@aliyun.com>
2026-05-13 15:29:10 +0800

    基础文本分块

---
+++ code/C2/study/study_02_character_splitter.py
@@ -0,0 +1,25 @@
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.document_loaders import TextLoader
+
+"""
+C2整体内容 文本分块
+"""
+file_path = "./data/C2/txt/蜂医.txt"
+
+# 加载文档
+loader = TextLoader(file_path=file_path , encoding="utf-8")
+doc = loader.load()
+
+# 文本分块
+characterTextSplitter = CharacterTextSplitter(
+    chunk_size = 100,
+    chunk_overlap = 100,
+)
+
+chunks = characterTextSplitter.split_documents(doc)
+
+# 查看输出内容
+print("总共分割的文档数量:", len(chunks))
+
+for i, e in enumerate(chunks[:10]):
+    print(f"当前的索引:{i}, 元素类型:{e.type}, 元素内容:{e.page_content}")
